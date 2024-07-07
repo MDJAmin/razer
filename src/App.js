296f35dd7c9a6ca40f0ffe5a6151dba2c7a8479a
MDJAmin <mohammadamindf@gmail.com>
2024-07-07 23:24:35 +0430

    Add the api base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import { commerce } from "./assets/lib/commerce";
 import { Products, Navbar } from "./components/index";
 
 export default function App() {
+  const [products, setProducts] = useState([]);
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  console.log(products);
   return (
     <div>
       <Navbar />
