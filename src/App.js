80de86760f48b2aaf68f9a3c23201b6f9da0df33
MDJAmin <mohammadamindf@gmail.com>
2024-07-07 22:50:14 +0430

    add index.js to components
    create a function that handle's the import stations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./App.css"
-import Products from "./components/Products/Products";
-import Navbar from "./components/Navbar/Navbar";
+// import Products from "./components/Products/Products";
+// import Navbar from "./components/Navbar/Navbar";
+
+import { Products, Navbar } from "./components/index"
 
 export default function App() {
   return (
