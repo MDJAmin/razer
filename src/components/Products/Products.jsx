328e5d22a07c1c97e818f7054c6d554e9ee3843d
MDJAmin <mohammadamindf@gmail.com>
2024-07-09 21:59:23 +0430

    add skeleton loading to products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Box, Grid, Skeleton } from "@mui/material";
 import Product from "./Product/Product";
 import "./style.css";
-import Atropos from "atropos/react";
+
 // const products = [
 //   { id: 1, name: "SwiftClick X1", price: "$19.99" },
 //   { id: 2, name: "ProMouse 360", price: "$27.49" },
@@ -11,21 +11,41 @@ import Atropos from "atropos/react";
 // ];
 
 export default function Products({ products, onAddToCart }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (products.length > 0) {
+      setLoading(false);
+    }
+  }, [products]);
+
   return (
-    <Atropos className="my-atropos">
     <main className="content">
       <div className="Title">
-      Customer Favorites !
+        Customer Favorites!
       </div>
       <div className="toolbar" />
-      <Grid container justify="center" spacing={6}>
-        {products.map((product) => (
-          <Grid item key={product.id} sx={12} md={4} lg={3}>
-            <Product product={product} onAddToCart={onAddToCart} />
-          </Grid>
-        ))}
-      </Grid>
+      {loading ? (
+        <Grid container justify="center" spacing={6}>
+          {Array.from(new Array(8)).map((_, index) => (
+            <Grid item key={index} sx={12} md={4} lg={3}>
+              <Skeleton variant="rectangular" width={350} height={300} sx={{ bgcolor: 'rgba(35, 181, 99, 0.07)' }}/>
+              <Box sx={{ pt: 0.5 }} >
+                <Skeleton sx={{ bgcolor: 'rgba(35, 181, 99, 0.07)' }}/>
+                <Skeleton width="60%" sx={{ bgcolor: 'rgba(35, 181, 99, 0.07)' }}/>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <Grid container justify="center" spacing={6}>
+          {products.map((product) => (
+            <Grid item key={product.id} sx={12} md={4} lg={3}>
+              <Product product={product} onAddToCart={onAddToCart} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
     </main>
-    </Atropos>
   );
 }
