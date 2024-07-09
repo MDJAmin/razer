e1bec7a0fbd5b26d545e6ae51f9243e6c9c277cb
MDJAmin <mohammadamindf@gmail.com>
2024-07-09 21:43:22 +0430

    complete the Offers section

--- src/components/Offers/Offers.jsx
+++ src/components/Offers/Offers.jsx
@@ -1,37 +1,83 @@
-import React from "react";
-import offerBg from "../../assets/Offers.png";
+// import React from "react";
+// import offerBg from "../../assets/Offers.png";
+// import "./style.css";
+// export default function Offers() {
+//   return (
+//     <div className="Offer-section">
+//       <div>
+//         <img className="bg-image" src={offerBg} alt="" />
+//       </div>
+//       <div className="card-items-description">
+//         <a href="https://www.razer.com/eu-en/gaming-laptops/razer-blade-14?utm_source=google&utm_medium=google-shopping&utm_campaign=240701_RS_SYS_MID_NL_EGR-systems_TR&CID=240701_RS_SYS_MID_NL_EGR-systems_TR&gad_source=1&gclid=Cj0KCQjwv7O0BhDwARIsAC0sjWNQq202TM7sdV1dags9kBQxgWqkwnxqyM1NUMshKeKfoNXb6Ye_bZIaAs3UEALw_wcB">
+//           SHOP NOW
+//         </a>
+//       </div>
+//     </div>
+//   );
+// }
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Offers, Offers1, Offers2, Offers3, Offers4 } from "./index";
+import { Pagination } from "swiper/modules";
+import "swiper/css/pagination";
+import "swiper/css";
 import "./style.css";
-export default function Offers() {
+
+export default function App() {
   return (
-    <div className="Offer-section">
-      <div className="card-items-offers">
-        <p>45% OFF</p>
-        <p>
-          subtle
-          <span>grin</span>
-        </p>
-        <p>13 jul to 13 Aug</p>
-      </div>
-      <div>
-        <img className="bg-image" src={offerBg} alt="" />
-      </div>
-      <div className="card-items-description">
-        <p>Limited-Time Discounts</p>
-        <p>Summer Sale</p>
-        <p>
-          "Experience incredible savings with our exclusive offer: Enjoy a
-          generous 20% discount on all products! This special discount is
-          available for a limited time only, running from Friday, July 12th
-          through Sunday, July 14th. Don't miss your chance to save on our
-          top-quality items. Whether you're stocking up on essentials or
-          treating yourself to something special, now is the perfect time to
-          shop. Hurry in and take advantage of this limited-time offer before it
-          expires!"
-        </p>
-        <a href="https://www.razer.com/eu-en/gaming-laptops/razer-blade-14?utm_source=google&utm_medium=google-shopping&utm_campaign=240701_RS_SYS_MID_NL_EGR-systems_TR&CID=240701_RS_SYS_MID_NL_EGR-systems_TR&gad_source=1&gclid=Cj0KCQjwv7O0BhDwARIsAC0sjWNQq202TM7sdV1dags9kBQxgWqkwnxqyM1NUMshKeKfoNXb6Ye_bZIaAs3UEALw_wcB">
-          SHOP NOW
-        </a>
-      </div>
-    </div>
+    <>
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="firstEl">
+            <a href="https://www.razer.com/eu-en/gaming-headsets/Razer-Kraken-Kitty-V2/RZ04-04730200-R3M1">
+              {" "}
+              <img src={Offers} alt="" />
+            </a>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <div>
+            <a href="https://www.razer.com/eu-en/gaming-headsets/Razer-Kraken-Kitty-V2/RZ04-04730200-R3M1">
+              {" "}
+              <img src={Offers1} alt="" />
+            </a>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <div>
+            <a href="https://www.razer.com/eu-en/gaming-headsets/Razer-Kraken-Kitty-V2/RZ04-04730200-R3M1">
+              {" "}
+              <img src={Offers2} alt="" />
+            </a>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <div>
+            <a href="https://www.razer.com/eu-en/gaming-headsets/Razer-Kraken-Kitty-V2/RZ04-04730200-R3M1">
+              {" "}
+              <img src={Offers3} alt="" />
+            </a>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <div>
+            <a href="https://www.razer.com/eu-en/gaming-headsets/Razer-Kraken-Kitty-V2/RZ04-04730200-R3M1">
+              {" "}
+              <img src={Offers4} alt="" />
+            </a>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 }
---
+++ src/components/Offers/index.js
@@ -0,0 +1,6 @@
+export { default as Offers } from "../../assets/Offers.png";
+export { default as Offers1 } from "../../assets/Offers(1).png";
+export { default as Offers2 } from "../../assets/Offers(2).png";
+export { default as Offers3 } from "../../assets/Offers(3).png";
+export { default as Offers4 } from "../../assets/Offers(4).png";
+
